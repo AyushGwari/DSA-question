d8b900d9ce9b7e9aa05ed9b9b8a431fbaea7bf8a
ayush <ayushgwari121@gmail.com>
2023-03-23 20:59:35 +0530

    second commit

--- findDuplicateNum.cpp
+++ findDuplicateNum.cpp
@@ -2,18 +2,35 @@
 using namespace std;
 
 int findDuplicateNum(vector<int>& nums) {
-        unordered_map<int,int> mp;
-        for(int i=0;i<nums.size();i++){
-            mp[nums[i]]++;
+        // unordered_map<int,int> mp;
+        // for(int i=0;i<nums.size();i++){
+        //     mp[nums[i]]++;
+        // }
+        // int ans = 0;
+        // for(auto m: mp){
+        //     if(m.second>1){
+        //         ans = m.first;
+        //     }
+        // }
+        // return ans;
+
+        // # 2nd approach 
+
+        int slow = nums[0];
+        int fast = nums[0];
+        do{
+            slow = nums[slow];
+            fast = nums[nums[fast]];
+        }while(slow!=fast);
+        fast = nums[0];
+        while(fast!=slow){
+            slow = nums[slow];
+            fast = nums[fast];
         }
-        int ans = 0;
-        for(auto m: mp){
-            if(m.second>1){
-                ans = m.first;
-            }
-        }
-        return ans;
+        return slow;
     }
+
+
 int main(){
     int n;
     cin>>n;
